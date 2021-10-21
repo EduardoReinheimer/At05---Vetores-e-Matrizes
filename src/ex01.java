79a9a556a06a3f0591dfc528e0fd20a4bb4f902c
Eduardo Reinheimer <du428@hotmail.com>
2021-10-21 11:00:02 -0300

    Primeiro Import

---
+++ src/ex01.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int [] vetor = new int[5];
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.print("Digite o valor do vetor de posição " + (i+1) + ": ");
+			vetor[i] = input.nextInt();
+		}
+		
+		input.close();
+		
+		for(int i: vetor) {
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
---
+++ src/ex02.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ex02 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		int[] vetor = new int[10];
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Digite o valor do vetor de posição " + (i + 1) + ": ");
+			vetor[i] = input.nextInt();
+		}
+		
+		input.close();
+
+		for (int i = vetor.length - 1; i >= 0; i--) {
+			System.out.println(vetor[i]);
+		}
+
+	}
+
+}
